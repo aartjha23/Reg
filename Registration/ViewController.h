2d6674367cda5b67fbb5e93b2e97a09a17bf1a4d
Rajesh <sandeep@SANDEEPs-MacBook-Pro.local>
2015-06-05 12:46:45 +0530

    Initial Commit

---
+++ Registration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Registration
+//
+//  Created by Rajesh on 05/06/15.
+//  Copyright (c) 2015 Rajesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Registration/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Registration
+//
+//  Created by Rajesh on 05/06/15.
+//  Copyright (c) 2015 Rajesh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
